911a98ddaae5c6a02609a3a6e70ab4bf00790aaa
Didzis Tupureins <d.tupureins@inbox.lv>
2019-10-24 16:47:16 +0300

    Adding Icons

--- DarkTheme/ViewController.swift
+++ DarkTheme/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkButton: UIButton!
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func darkButtonTapped(_ sender: Any) {
+        if isOn{
+            self.isOn = false
+            self.darkButton.setTitle("Dark Theme On", for: .normal)
+            self.darkButton.setTitleColor(UIColor.white, for: .normal)
+            self.darkButton.setImage(UIImage(named: "mail.png"), for: .normal)
+            self.darkButton.tintColor = .white
+            self.darkButton.contentVerticalAlignment = .fill
+            self.darkButton.contentHorizontalAlignment = .fill
+            self.darkButton.imageEdgeInsets = UIEdgeInsets(top: 40, left: 0, bottom: 40, right: 0)
+            self.view.backgroundColor = UIColor.black
+        }else{
+            self.isOn = true
+            self.darkButton.setTitle("Dark Theme Of", for: .normal)
+            self.darkButton.setTitleColor(UIColor.black, for: .normal)
+            self.view.backgroundColor = UIColor.white
+        }
+    }
 }
 
